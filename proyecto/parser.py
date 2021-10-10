2cd9e041305c2860dd77f9a292da555035e4dfcd
Raul <raulcastellanosh@hotmail.com>
2021-10-09 21:46:34 -0500

    Add neuralgic points and add scopes and vars

--- proyecto/parser.py
+++ proyecto/parser.py
@@ -1,5 +1,10 @@
 import ply.yacc as yacc
+import sys
+from collections import deque
 from lexer import tokens, keywords
+from directories.scopes import Scopes_directory
+from directories.vars import Vars
+from utils import Data_types
 
 # symbol_table = {
 #     "mi_variable": 
@@ -11,28 +16,39 @@ from lexer import tokens, keywords
 
 # }
 
+# Program directory, this contains all functions and the main scope
+program_scopes = Scopes_directory()
+current_scope = ''
+# Used for declaration of many variables in one line
+# for example: let my_var1, my_var2 : int;
+vars_stack = deque()
+params_vars_stack = deque()
+
 # PROGRAM
 def p_program(p):
-    '''program : PROGRAM ID SEMI program_1'''
-    print('esto')
+    '''program : PROGRAM ID np_create_global SEMI vars program_1
+        |  PROGRAM ID np_create_global SEMI program_1'''
     p[0] = 'correct'
 
 def p_program_1(p):
-    '''program_1 : vars program_1
-        | function program_1
+    '''program_1 : function program_1
         | main_block'''
+    p[0] = None
+
 
 # VARS
 def p_vars(p):
-    '''vars : LET vars_prima_1'''
+    '''vars : vars_1'''
     p[0] = ('let', p[1])
 
+def p_vars_1(p):
+    '''vars_1 : LET vars_prima_1 vars_1
+        | LET vars_prima_1'''
 # var_1, var_2 : int;
 # var_1 : int;
 def p_vars_prima_1(p):
-    '''vars_prima_1 : ID COLON type SEMI
-        | ID COMMA vars_prima_1'''
-    print(p[1], p[3])
+    '''vars_prima_1 : ID COLON type np_add_vars SEMI
+        | ID  np_add_satck_vars COMMA vars_prima_1'''
     # añado la var a mi tabla de variables
 
 def p_type(p):
@@ -40,18 +56,22 @@ def p_type(p):
         | FLOAT type_1
         | CHAR type_1
         | BOOL type_1'''
-    p[0] = int
+    p[0] = p[1]
 
 def p_type_1(p):
     '''type_1 : LBRACKET expression RBRACKET
         | epsilon'''
 
 def p_function(p):
-    '''function : FUNCTION ID COLON return_type LPAREN params RPAREN block
-        | FUNCTION ID COLON return_type LPAREN RPAREN block'''
+    '''function : FUNCTION ID COLON return_type np_create_new_scope LPAREN RPAREN block
+        | FUNCTION ID COLON return_type np_create_new_scope LPAREN params RPAREN block
+        | FUNCTION ID COLON return_type np_create_new_scope LPAREN RPAREN vars block
+        | FUNCTION ID COLON return_type np_create_new_scope LPAREN params RPAREN vars block'''
+    p[0] = None
 
 def p_main_block(p):
-    '''main_block : MAIN LPAREN RPAREN block'''
+    '''main_block : MAIN np_create_main_scope LPAREN RPAREN block
+        | MAIN np_create_main_scope LPAREN RPAREN vars block'''
 
 def p_block(p):
     '''block : LBRACE statements RBRACE'''
@@ -59,10 +79,11 @@ def p_block(p):
 def p_return_type(p):
     '''return_type : VOID
         | type'''
+    p[0] = p[1]
 
 def p_params(p):
-    '''params : ID COLON type COMMA params
-        | ID COLON type'''
+    '''params : ID COLON type np_add_vars COMMA params
+        | ID COLON type np_add_vars'''
 
 def p_statements(p):
     '''statements : assignment statements1
@@ -75,7 +96,6 @@ def p_statements(p):
         | expression statements1
         | special_functions statements1'''
     p[0] = (p[1], p[2])
-    evaluate(p[0])
 
 def p_special_functions(p):
     '''special_functions : mean
@@ -212,15 +232,72 @@ def p_standard_deviation(p):
 
 def p_epsilon(p):
     '''epsilon : '''
-    print(p)
     p[0] = None
 
 def p_error(token):
     print(f"Syntax Error: {token.value!r}")
     print(token)
     token.lexer.skip(1)
+    sys.exit()
+
+
+# ======================================================================
+#                        Functions for scopes
+# ======================================================================
+
+def p_np_create_global(p):
+    '''np_create_global : '''
+    global program_scopes, current_scope
+    create_scope('program', Data_types['VOID'])
+
+def p_np_create_main_scope(p):
+    '''np_create_main_scope : '''
+    create_scope('main', Data_types['VOID'])
+
+# p[-4]   p[-3]p[-2]  p[-1]     p[0]
+# FUNCTION ID COLON return_type np_create_new_scope LPAREN RPAREN  block
+#  p[-4]  p[-3] p[-2]  p[-1]    p[0]
+# FUNCTION ID COLON return_type np_create_new_scope LPAREN params RPAREN  block
+def p_np_create_new_scope(p):
+    '''np_create_new_scope : '''
+    global program_scopes, current_scope
+    function_id = p[-3]
+    return_type = p[-1]
+    create_scope(function_id, return_type)
+
+def create_scope(scope_id, return_type):
+      # Create the global scope
+    global program_scopes, current_scope
+    print('New Scope', scope_id)
+    program_scopes.add_new_scope(scope_id, return_type, Vars(scope_id))
+    current_scope = scope_id
+
+
+
+# For example
+# let my_var, my_var2, my_last_var : int NP_ADD_VARS ;
+# function miSuperFunction : void (uno:int, dos:bool)
+# function miSuperFunction : void (uno:int np_add_vars, dos:bool np_add_vars)
+
+# Here is adding to stack my_var, my_var2
+def p_np_add_satck_vars (p):
+    '''np_add_satck_vars : '''
+    global vars_stack
+    vars_stack.append(p[-1])
+
+# Here is adding to stack my_last_var
+def p_np_add_vars(p):
+    '''np_add_vars : '''
+    global program_scopes, current_scope, vars_stack
+    # Add the last var to stack o the var
+    var_id = p[-3]
+    vars_stack.append(p[-3])
+    vars_type = p[-1]
+
+    while vars_stack:
+        current_scope_vars = program_scopes.get_vars_table(current_scope)
+        current_scope_vars.add_new_var(vars_stack[0], vars_type)
+        vars_stack.popleft()
+
 
 parser = yacc.yacc()
-
-def evaluate(p):
-    print('Evaluate:', p)
